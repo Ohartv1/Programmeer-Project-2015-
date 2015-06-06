03c8db335d98b27d6729234999e3585ce909fa2c
Ohartv1 <ohartveldt@gmail.com>
2015-06-06 17:41:22 +0200

    selenium update in crawler

--- ArticleCrawler.py
+++ ArticleCrawler.py
@@ -1,18 +1,24 @@
 
-import json
+import json, time, unicodedata
 from pattern.web import URL, DOM, plaintext
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-TARGET_URL = "http://apps.webofknowledge.com"
-#TARGET_query = "/full_record.do?product=UA&search_mode=GeneralSearch&qid=8&SID=P1xkMFYxAIRfFuaGs4Z&page=1&doc=1"
-TARGET_query = "/full_record.do?product=UA&search_mode=GeneralSearch&qid=8&doc=1"
-test_query = "/CitingArticles.do?product=WOS&REFID=22967726&search_mode=CitingArticles&parentProduct=UA&parentQid=8&parentDoc=1&excludeEventConfig=ExcludeIfFromFullRecPage"
 
-def crawl(article):
 
-    # article = list_of_article[-1]
+
+
+
+
+
+
+
+TARGET = "http://apps.webofknowledge.com"
+
+def crawl(browser, article):
 
     # temp_list = []
-    # for url in cited_by(article[link_cited]):
+    # for url in cited_by(browser, first.get("link_cited")):
         # make a dom
         # scrape 
         # place in temp_list
@@ -20,31 +26,62 @@ def crawl(article):
 
     # list_of_articles = list_of_articles + temp_list
 
-    return list_of_articles
-
-def cited_by(query):
-    """
-    """
-    url = URL(TARGET_URL + query)
-    print url
-    html = url.download()
-
-
-    with open('test.html', 'w') as f:
-        f.write(html)
-
-
-    # Parse the HTML file into a DOM representation
-    dom = DOM(html)
-    print dom
     
-    list_of_querys = []
-    for link in dom.by_class("smalV110"):
-        #print link
-        link = link.attrs.get("href", "")
-        list_of_querys.append(link)
+    list_of_articles = []
+    for each in cited_by(browser, article.get("link_cited")):
+        # Download the HTML file of starting point
+        url = URL(each)
+        html = url.download()
 
-    return list_of_querys
+        # Parse the HTML file into a DOM representation
+        dom = DOM(html)
+        current = scrape_reference(dom)
+
+        list_of_articles.append(current)
+
+        temp_list = list_of_articles + crawl(browser, current)
+
+
+
+
+    return list_of_articles
+    
+
+def cited_by(browser, url):
+    """
+    """
+    browser.get(url)
+    content = browser.page_source
+    page_bottom = browser.find_element_by_id("pageCount.bottom").text
+
+
+    list_of_urls = []
+
+    while True:
+        links = browser.find_elements_by_class_name('smallV110')
+
+        for link in links:  
+            href = link.get_attribute("href")
+            list_of_urls.append(href)
+
+        stop = browser.find_element_by_class_name("goToPageNumber-input")
+        stop = stop.get_attribute("value")
+
+        if stop == page_bottom:
+            break
+
+        browser.find_element_by_class_name("paginationNext").click()
+
+    return list_of_urls
+
+def give_start(browser, website, string_for_search):
+    browser.get(website)
+    elem = browser.find_element_by_id("value(input1)")
+    elem.send_keys(string_for_search)
+    elem.send_keys(Keys.RETURN)
+    link = browser.find_element_by_class_name("smallV110")
+    href = link.get_attribute("href")
+    return href
 
 
 def scrape_reference(dom):
@@ -55,49 +92,50 @@ def scrape_reference(dom):
     data_dict = {}
 
     # title
-    article = dom
-    title_parts = []
-    for line in article.by_class("title"):
+    for line in dom.by_class("title"):
         for part in line.by_class("hitHilite"):
             article_name = plaintext(line.content.encode("ascii", "ignore"))
-
-    data_dict.update({"title": article_name})
+            data_dict.update({"title": article_name})
 
      
-    # DOI
-    doi = plaintext(article.by_tag("value")[5].content)
-    data_dict.update({"doi": doi})
+        # DOI
+        doi = plaintext(dom.by_tag("value")[5].content)
+        data_dict.update({"doi": doi})
 
     # Cited link
-    for link in article.by_attr(title="View all of the articles that cite this one"):
+    for link in dom.by_attr(title="View all of the articles that cite this one"):
         link = link.attrs.get("href", "")
-        data_dict.update({"link_cited": link})
+        data_dict.update({"link_cited": TARGET + link})
 
+
+
+    print data_dict
     return data_dict
 
 
 if __name__ == '__main__':
-    # Download the HTML file of starting point
+    # create webbrowser
+    browser = webdriver.Firefox()
 
-    url = URL(TARGET_URL + TARGET_query)
+    START_URL = give_start(browser, TARGET, "Determinants and mechanisms in ego identity development: A review and synthesis")
+
+    # Download the HTML file of starting point
+    url = URL(START_URL)
     html = url.download()
 
     # Parse the HTML file into a DOM representation
     dom = DOM(html)
-    #print dom
 
     # scrape:
     first = scrape_reference(dom)
 
     # List of all articles
     list_of_articles = []
-    list_of_articles = list_of_articles + crawl(first)
+    list_of_articles = list_of_articles + crawl(browser, first)
 
-    # List of querys to crawl
-    querys = cited_by(test_query)
-    #print querys
+    browser.close()
 
     # parse and write to JSON
-    out = json.dumps(first)
+    out = json.dumps(list_of_articles)
     f = open("Data.json", "w")
     f.write(out)
