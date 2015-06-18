e37ea5383fb9b1bbcf395d738852671153941e31
Ohartv1 <ohartveldt@gmail.com>
2015-06-18 17:50:56 +0200

    update

--- main.js
+++ main.js
@@ -6,13 +6,29 @@
 
 "use strict";
 
-var width  = 960;           // width of svg image
-var height = 560;           // height of svg image
+var width  = 1010;           // width of svg image
+var height = 460;           // height of svg image
 var margin = 20;            // amount of margin around plot area
 var pad = margin / 2;       // actual padding amount
 var radius = 4;             // fixed node radius
 var yfixed = pad + radius;  // y position for all nodes
 var xfixed = width / 2;
+
+var color = d3.scale.category20();
+
+var yscale = d3.scale.linear()
+    .range([radius, height - margin - radius]);
+
+// scale to generate radians (just for lower-half of circle)
+var radians = d3.scale.linear()
+    .range([Math.PI / 180, Math.PI]);
+
+// path generator for arcs (uses polar coordinates)
+var arc = d3.svg.line.radial()
+    .interpolate("basis")
+    .tension(0)
+    .angle(function(d) { return radians(d); });
+
 
 /* HELPER FUNCTIONS */
 
@@ -26,13 +42,12 @@ function addTooltip(circle) {
     var tooltip = d3.select("#plot")
         .append("text")
         .text(text)
-        .attr("x", x)
-        .attr("y", y)
-        .attr("dy", -r * 2)
-        .attr("id", "tooltip");
-
-        tooltip.attr("text-anchor", "end");
-        tooltip.attr("dy", 0);
+        .attr("x", x - r)
+        .attr("y", y + r / 2)
+        .attr("dy", r)
+        .attr("id", "tooltip")
+        .attr("text-anchor", "end")
+        .attr("dy", 0);
 }
 
 /* MAIN DRAW METHOD */
@@ -47,10 +62,10 @@ function arcDiagram(graph) {
         .attr("height", height);
 
     // draw border around svg image
-    // svg.append("rect")
-    //     .attr("class", "outline")
-    //     .attr("width", width)
-    //     .attr("height", height);
+    svg.append("rect")
+        .attr("class", "outline")
+        .attr("width", width)
+        .attr("height", height);
 
     // create plot area within svg image
     var plot = svg.append("g")
@@ -63,6 +78,9 @@ function arcDiagram(graph) {
         d.target = isNaN(d.target) ? d.target : graph.nodes[d.target];
     });
 
+
+    d3.select("input").on("change", change);
+
     // must be done AFTER links are fixed
     linearLayout(graph.nodes);
 
@@ -73,23 +91,24 @@ function arcDiagram(graph) {
     drawNodes(graph.nodes);
 }
 
+
+
 // Layout nodes linearly, sorted by...
 function linearLayout(nodes) {
     // sort nodes by group
     nodes.sort(function(a, b) {
         // return a.group - b.group;       //by group
-        return b.generation - a.generation //by generation
-        // return a.num_cit - b.num_cit;   //by citations 
-    })
+        // return b.generation - a.generation //by generation
+        return a.num_cit - b.num_cit;   //by citations 
+    });
 
     // used to scale node index to x position
     var xscale = d3.scale.linear()
         .domain([0, nodes.length - 1])
         .range([radius, width - margin - radius]);
 
-    var yscale = d3.scale.linear()
-        .domain([0, nodes.length - 1])
-        .range([radius, height - margin - radius]);
+    yscale.domain([0, nodes.length - 1]);
+
 
     // calculate pixel location for each node
     nodes.forEach(function(d, i) {
@@ -100,11 +119,63 @@ function linearLayout(nodes) {
     });
 }
 
+
+// change linear layout with different sorting
+function change(svg) {
+
+    d3.select("#plot").selectAll(".node")
+        .sort(this.checked
+        ? function(a, b) { return b.generation - a.generation; }
+        : function(a, b) { return a.num_cit - b.num_cit; })
+        .each(function(d, i){
+            d.y = yscale(i);
+        })
+
+
+    // source: http://bl.ocks.org/mbostock/3885705
+    var transition = d3.transition().duration(800),
+        delay = function(d, i) { return i * 50; };
+
+
+    transition.selectAll(".node")
+        .delay(delay)
+        .style("fill",   function(d, i) { return color(d.generation); })
+        .attr("cy", function(d, i) { return yscale(i); });
+
+
+    transition.selectAll(".link")
+        .delay(delay)
+        .attr("transform", function(d, i) {
+            // arc will always be drawn around (0, 0)
+            // shift so (0, 0) will be between source and target
+            var xshift = xfixed;
+            var yshift = d.source.y + (d.target.y - d.source.y) / 2;
+            return "translate(" + xshift + ", " + yshift + ")";
+        })
+        .attr("d", function(d, i) {
+            // get y distance between source and target
+            var ydist = Math.abs(d.source.y - d.target.y);
+
+            // set arc radius based on x distance
+            arc.radius(ydist / 2);
+
+            // want to generate 1/3 as many points per pixel in y direction
+            var points = d3.range(0, Math.ceil(ydist / 3));
+
+            // set radian scale domain
+            radians.domain([0, points.length - 1]);
+
+            // return path for arc
+            return arc(points);
+        })
+        .attr('stroke', 'red')
+}
+
+
 // Draws nodes on plot
 function drawNodes(nodes) {
     // used to assign nodes color by group
-    var color = d3.scale.category20();
-
+    
     d3.select("#plot").selectAll(".node")
         .data(nodes)
         .enter()
@@ -113,23 +184,15 @@ function drawNodes(nodes) {
         .attr("id", function(d, i) { return d.name; })
         .attr("cx", function(d, i) { return d.x; })
         .attr("cy", function(d, i) { return d.y; })
-        .attr("r",  function(d, i) { return radius * (d.num_cit + 1) * 0.5; })
-        .style("fill",   function(d, i) { return color(1); })
+        .attr("r",  function(d, i) { return radius; })
+        .style("fill",   function(d, i) { return color(d.generation); })
         .on("mouseover", function(d, i) { addTooltip(d3.select(this)); })
         .on("mouseout",  function(d, i) { d3.select("#tooltip").remove(); });
 }
 
-// Draws nice arcs for each link on plot
+// Draws arcs for each link on plot
 function drawLinks(links) {
-    // scale to generate radians (just for lower-half of circle)
-    var radians = d3.scale.linear()
-        .range([Math.PI / 180, 2 * Math.PI / 2]);
 
-    // path generator for arcs (uses polar coordinates)
-    var arc = d3.svg.line.radial()
-        .interpolate("basis")
-        .tension(0)
-        .angle(function(d) { return radians(d); });
 
     // add links
     d3.select("#plot").selectAll(".link")
@@ -161,3 +224,4 @@ function drawLinks(links) {
             return arc(points);
         });
 }
+
