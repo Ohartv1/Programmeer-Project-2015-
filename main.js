e7e31c9cdd60d5c0073893267092df085d2ad05b
Ohartv1 <ohartveldt@gmail.com>
2015-06-24 13:48:41 +0200

    update

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var radius = 4;             // fixed node radius
 var yfixed = pad + radius;  // y position for all nodes
 var xfixed = width / 2;
 
-var color = d3.scale.category10();
+var color = d3.scale.category20();
 
 var yscale = d3.scale.linear()
     .range([radius, height - margin - radius]);
@@ -31,30 +31,10 @@ var arc = d3.svg.line.radial()
 
 //Toggle stores whether the highlighting is on
 var toggle = 0;
-//Create an array logging what is connected to what
-var linkedByYPosition = {};
+var toggle1 = 0;
 
 
 
-/* HELPER FUNCTIONS */
-
-// Generates a tooltip for a SVG circle element based on its ID
-function addTooltip(circle) {
-    var x = parseFloat(circle.attr("cx"));
-    var y = parseFloat(circle.attr("cy"));
-    var r = parseFloat(circle.attr("r"));
-    var text = circle.attr("id");
-
-    var tooltip = d3.select("#plot")
-        .append("text")
-        .text(text)
-        .attr("x", x - r)
-        .attr("y", y + r / 2)
-        .attr("dy", r)
-        .attr("id", "tooltip")
-        .attr("text-anchor", "end")
-        .attr("dy", 0);
-}
 
 /* MAIN DRAW METHOD */
 
@@ -98,14 +78,12 @@ function arcDiagram(graph) {
     drawNodes(graph);
 }
 
-
+/* STATIC FUNCTIONS*/
 
 // Layout nodes linearly, sorted by...
 function linearLayout(nodes) {
     // sort nodes by group
     nodes.sort(function(a, b) {
-        // return a.group - b.group;       //by group
-        // return b.generation - a.generation //by generation
         return a.num_cit - b.num_cit;   //by citations 
     });
 
@@ -119,61 +97,9 @@ function linearLayout(nodes) {
 
     // calculate pixel location for each node
     nodes.forEach(function(d, i) {
-        // d.x = xscale(i); // horizontale weergave
-        // d.y = yfixed;    //
         d.x = xfixed;       // vertikale weergave
         d.y = yscale(i);    //
     });
-}
-
-
-// change linear layout with different sorting
-function change() {
-
-    d3.select("#plot").selectAll(".node")
-        .sort(this.checked
-        ? function(a, b) { return b.generation - a.generation; }
-        : function(a, b) { return a.num_cit - b.num_cit; })
-        .each(function(d, i){
-            d.y = yscale(i);
-        })
-
-
-    // source: http://bl.ocks.org/mbostock/3885705
-    var transition = d3.transition().duration(800),
-        delay = function(d, i) { return i * 50; };
-
-
-    transition.selectAll(".node")
-        .delay(delay)
-        .attr("cy", function(d, i) { return yscale(i); });
-
-
-    transition.selectAll(".link")
-        .delay(delay)
-        .attr("transform", function(d, i) {
-            // arc will always be drawn around (0, 0)
-            // shift so (0, 0) will be between source and target
-            var xshift = xfixed;
-            var yshift = d.source.y + (d.target.y - d.source.y) / 2;
-            return "translate(" + xshift + ", " + yshift + ")";
-        })
-        .attr("d", function(d, i) {
-            // get y distance between source and target
-            var ydist = Math.abs(d.source.y - d.target.y);
-
-            // set arc radius based on x distance
-            arc.radius(ydist / 2);
-
-            // want to generate 1/3 as many points per pixel in y direction
-            var points = d3.range(0, Math.ceil(ydist / 3));
-
-            // set radian scale domain
-            radians.domain([0, points.length - 1]);
-
-            // return path for arc
-            return arc(points);
-        })
 }
 
 
@@ -181,42 +107,39 @@ function change() {
 function drawNodes(graph) {
     // used to assign nodes color by group
     
-    d3.select("#plot").selectAll(".node")
+    var tmp = d3.select("#plot").selectAll(".node")
         .data(graph.nodes)
         .enter()
-        .append("circle")
+        .append("g")
         .attr("class", "node")
+        .on("click", function(p, i) { makeImportant(d3.select(this)); })
+
+    tmp.append("circle")
+        .attr("class", "circle" )
         .attr("id", function(d, i) { return d.name; })
         .attr("cx", function(d, i) { return d.x; })
         .attr("cy", function(d, i) { return d.y; })
         .attr("r",  function(d, i) { return radius; })
-        .style("fill",   function(d, i) { return color(d.generation); })
+        .attr("toggle", 0)
+        .style("fill", "#993404") 
         .on("mouseover", function(d, i) { addTooltip(d3.select(this)); })
         .on("mouseout",  function(d, i) { d3.select("#tooltip").remove(); })
-        .on("click", function(p, i){
-            highLight(graph, i);
-        });
-        
+        .on("dblclick", function(p, i){ highLight(graph, i); });
 
-    // save linked articles to highlight at later moment
-    // for (var i = 0; i < graph.nodes.length; i++) {
-    //     linkedByIndex[i + "," + i] = 1;
-    // };
-
-    graph.nodes.forEach(function (d) {
-        linkedByYPosition[parseInt(d.y) + "," + parseInt(d.y)] = 1;
-    });
-
-    // console.log(linkedByIndex);
-    graph.links.forEach(function (d) {
-        linkedByYPosition[parseInt(d.source.y) + "," + parseInt(d.target.y)] = 1;
-    });
-
+    tmp.append("text")
+        .attr("class", "text")
+        .text(function(d, i) { return d.name; })
+        .attr("x", function(d, i) { return d.x - radius; } )
+        .attr("y", function(d, i) { return d.y + radius / 2; })
+        .attr("text-anchor", "end")
+        .attr("dy", 0)
+        .attr("class", "important")
+        .style("visibility", "hidden")        
 }
+
 
 // Draws arcs for each link on plot
 function drawLinks(links) {
-
 
     // add links
     d3.select("#plot").selectAll(".link")
@@ -250,14 +173,48 @@ function drawLinks(links) {
 }
 
 
-//This function looks up whether a pair are neighbours
-function neighboring(a, b) {
-    return linkedByYPosition[parseInt(a.y) + "," + parseInt(b.y)];
+/* DYNAMIC FUNCTIONS */
+
+// Generates a tooltip for a SVG circle element based on its ID
+function addTooltip(node) {
+    var x = parseFloat(node.attr("cx"));
+    var y = parseFloat(node.attr("cy"));
+    var r = parseFloat(node.attr("r"));
+    var text = node.attr("id");
+
+
+    var tooltip = d3.select("#plot")
+        .append("text")
+        .text(text)
+        .attr("x", x - r)
+        .attr("y", y + r / 2)
+        .attr("dy", r)
+        .attr("id", "tooltip")
+        .attr("text-anchor", "end")
+        .attr("dy", 0);
 }
 
+
+// Lowers the opacity of the phericial nodes and links of the selection
 function highLight(graph, i) {
     // Source: http://www.coppelia.io/2014/07/an-a-to-z-of-extra-features-for-the-d3-force-layout/ 
     //
+
+    //This function looks up whether a pair are neighbours
+    function neighboring(a, b) {
+        return linkedByYPosition[a.y + "," + b.y];
+    }
+
+    //Create an array logging what is connected to what
+    var linkedByYPosition = {};
+
+    d3.selectAll(".node").each(function (d) {
+        linkedByYPosition[d.y + "," + d.y] = 1;
+    });
+
+    d3.selectAll(".link").each(function (d) {
+        linkedByYPosition[d.source.y + "," + d.target.y] = 1;
+    });
 
     if (toggle == 0) {
         //Reduce the opacity of all but the neighbouring nodes
@@ -265,12 +222,12 @@ function highLight(graph, i) {
 
         d3.selectAll(".node").each( function(a) {
             d3.select(this).style("opacity", function (o) {
-                return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1; });
+                return neighboring(d, o) | neighboring(o, d) ? 1 : 0.3; });
         }); 
 
         d3.selectAll(".link").each( function(a) {
             d3.select(this).style("opacity", function (o) {
-                return neighboring(d, o.source) | neighboring(d, o.target) ? 1 : 0.1; });
+                return d.y == o.source.y | d.y == o.target.y ? 1 : 0.3; });
         });
 
         //Reduce the op
@@ -282,10 +239,79 @@ function highLight(graph, i) {
         d3.selectAll(".link").style("opacity", 1);
         toggle = 0; 
     }
+}
 
+
+// makes a select node recognisable
+function makeImportant(node){
+
+    
+    node.select("circle").attr("toggle", function(o) {
+        return node.select("circle").attr("toggle") == 0 ? 1 : 0 ;
+    });
+
+
+    node.each( function(a) {
+        node.select(".important").style("visibility", function (o) {
+            console.log(node.select("circle").attr("toggle"));
+            return node.select("circle").attr("toggle") == 1 ? "visible" : "hidden"; });
+    }); 
 
 }
 
+
+// change linear layout with different sorting
+function change() {
+
+    d3.select("#plot").selectAll(".node")
+        .sort(this.checked
+        ? function(a, b) { return b.generation - a.generation; }
+        : function(a, b) { return a.num_cit - b.num_cit; })
+        .each(function(d, i){
+            d.y = yscale(i);
+        })
+
+
+    // source: http://bl.ocks.org/mbostock/3885705
+    var transition = d3.transition().duration(800),
+        delay = function(d, i) { return i * 50; };
+
+
+    transition.selectAll(".circle")
+        .delay(delay)
+        .attr("cy", function(d, i) { return yscale(i); });
+
+    transition.selectAll(".important")
+        .delay(delay)
+        .attr("y", function(d, i) { return yscale(i) + radius / 2; });
+
+
+    transition.selectAll(".link")
+        .delay(delay)
+        .attr("transform", function(d, i) {
+            // arc will always be drawn around (0, 0)
+            // shift so (0, 0) will be between source and target
+            var xshift = xfixed;
+            var yshift = d.source.y + (d.target.y - d.source.y) / 2;
+            return "translate(" + xshift + ", " + yshift + ")";
+        })
+        .attr("d", function(d, i) {
+            // get y distance between source and target
+            var ydist = Math.abs(d.source.y - d.target.y);
+
+            // set arc radius based on x distance
+            arc.radius(ydist / 2);
+
+            // want to generate 1/3 as many points per pixel in y direction
+            var points = d3.range(0, Math.ceil(ydist / 3));
+
+            // set radian scale domain
+            radians.domain([0, points.length - 1]);
+
+            // return path for arc
+            return arc(points);
+        })
+}
 
 
 
